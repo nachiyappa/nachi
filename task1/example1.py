bd8513a4277a10335f1b55ba823c64de22fc0339
nachiyappa <suryanachi33@gmail.com>
2023-11-10 13:37:54 +0530

    second commit

--- task1/example1.py
+++ task1/example1.py
@@ -1,4 +1,17 @@
-x,y,z=10,20,30
-print("x=",x)
-print("y=",y)
-print("z=",z)
+def add():
+    x, y, z = 10, 20, 30
+    print("x=", x)
+    print("y=", y)
+    print("z=", z)
+
+
+def sub():
+    x, y, z = 10, 20, 30
+    print("x=", x)
+    print("y=", y)
+    print("z=", z)
+
+
+if __name__ == "__main__":
+    add()
+    sub()
